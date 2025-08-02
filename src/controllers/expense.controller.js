1f2208634b232ae68ea4b2f51dce0f2b96700298
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-08-02 12:41:57 +0300

    feat: expens update, delete, getAllexpenses, getAllexpensesByme donw

--- src/controllers/expense.controller.js
+++ src/controllers/expense.controller.js
@@ -1,10 +1,61 @@
 import { Expens } from '../models/expense.model.js';
+import ApiError from '../utils/apiError.js';
 import ApiSuccess from '../utils/apiSuccess.js';
 import asyncHandler from '../utils/asyncHandler.js';
 
 const createMyexpense = asyncHandler(async (req, res) => {
-  const expense = await Expens.create({ ...req.body, createdBy: req.user._id });
+  const expense = await Expens.create({
+    ...req.body,
+    paidBy: req.user._id,
+    createdBy: req.user._id,
+  });
   return res.status(200).json(ApiSuccess.ok('expense created', expense));
 });
 
-export { createMyexpense };
+const updateMyexpense = asyncHandler(async (req, res) => {
+  const { expenseId } = req.params;
+  const existsExpens = await Expens.findById(expenseId);
+  if (!existsExpens) throw ApiError.notFound('expens is not found');
+
+  const expens = await Expens.findByIdAndUpdate(
+    { _id: expenseId, createdBy: req.user._id },
+    { $set: { ...req.body } },
+    { new: true },
+  );
+  return res.status(200).json(ApiSuccess.ok('expens update', expens));
+});
+
+const deleteMyexpens = asyncHandler(async (req, res) => {
+  const { expenseId } = req.params;
+  const expens = await Expens.findOneAndDelete({
+    _id: expenseId,
+    createdBy: req.user._id,
+  });
+  if (!expens) throw ApiError.notFound('expens not found');
+  return res.status(200).json(ApiSuccess.ok('expens deleted', expens));
+});
+
+const getAllexpense = asyncHandler(async (req, res) => {
+  const expenses = await Expens.find({ createdBy: req.user._id });
+  if (expenses.length == 0) throw ApiError.notFound('expenses not found');
+  return res.status(200).json(ApiSuccess.ok('expenses fitched', expenses));
+});
+
+const getAllExpensesPaidByme = asyncHandler(async (req, res) => {
+  const expenses = await Expens.find({
+    createdBy: req.user._id,
+    paidBy: req.user._id,
+  });
+  if (expenses.length == 0) throw ApiError.notFound('expenses not found');
+  return res
+    .status(200)
+    .json(ApiSuccess.ok('all expenses fitched paid By me', expenses));
+});
+
+export {
+  createMyexpense,
+  updateMyexpense,
+  deleteMyexpens,
+  getAllexpense,
+  getAllExpensesPaidByme,
+};
--- src/routes/expense.route.js
+++ src/routes/expense.route.js
@@ -1,6 +1,12 @@
 import e from 'express';
 import auth from '../middlewares/auth.middleware.js';
-import { createMyexpense } from '../controllers/expense.controller.js';
+import {
+  createMyexpense,
+  deleteMyexpens,
+  getAllexpense,
+  getAllExpensesPaidByme,
+  updateMyexpense,
+} from '../controllers/expense.controller.js';
 import ValidationMiddleware from '../middlewares/validation.middleware.js';
 import { createIndividualExpensSchema } from '../validators/expens.validator.js';
 const router = e.Router();
@@ -11,6 +17,19 @@ router
     auth,
     ValidationMiddleware(createIndividualExpensSchema),
     createMyexpense,
+  )
+  .get(auth, getAllexpense);
+
+router.route('/expenses/individual/paidByme').get(auth, getAllExpensesPaidByme);
+router
+  .route('/expenses/individual/update/:expenseId')
+  .post(
+    auth,
+    ValidationMiddleware(createIndividualExpensSchema),
+    updateMyexpense,
   );
+router
+  .route('/expenses/individual/delete/:expenseId')
+  .delete(auth, deleteMyexpens);
 
 export default router;
